813afbc95c271c62432eafc7a489abf1ee2a1aee
gaoyuan <gaoyuan_flying@163.com>
2021-01-11 19:31:06 +0800

    首页的编写

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,8 +1,23 @@
 <template>
 	<view class="content"> 
-		<block v-for="(item, index) in list" :key="index">
-			<home-list :item="item" :index="index"></home-list>
-		</block>
+		<scroll-view :scroll-with-animation="true" :scroll-x="true" class="uni-swiper-tab" :scroll-into-view="'scroll_tab_'+tabIndex">
+			<block v-for="(tab, index) in tabBars" :key="tab.id">
+				<view @click="tabTap(index)" class="swiper-tab-list" :class="{active: index == tabIndex}" :id="'scroll_tab_' + index">
+					{{tab.name}}
+				</view>
+			</block>
+		</scroll-view>
+		<view class="content-view">
+			<swiper @change="swiperChange" class="swiper-outer-container" :style="{height: contentHeight + 'px' }" :current="tabIndex">
+				<swiper-item class="" v-for="(swiperItem, swiperIndex) in dataList" :key="swiperIndex">
+					<scroll-view :scroll-y="true" class="list" :style="{height: contentHeight + 'px' }">
+						<block v-for="(item, index) in swiperItem.list" :key="index">
+							<home-list :item="item" :index="index"></home-list>
+						</block>
+					</scroll-view>
+				</swiper-item>
+			</swiper>
+		</view>
 	</view>
 </template>
 
@@ -14,50 +29,280 @@
 		},
 		data() {
 			return {
-				list: [
+				contentHeight: 500,
+				tabIndex: 0,
+				tabBars: [
+					{ name: '关注',id: 'guanzhu' },
+					{ name: '推荐',id: 'tuijian' },
+					{ name: '体育',id: 'tiyu' },
+					{ name: '热点',id: 'redian' },
+					{ name: '军事',id: 'junshi' },
+					{ name: '财经',id: 'caijing' },
+				],
+				dataList: [
 					{
-						userpic: '../../static/demo/userpic/12.jpg',
-						username: '昵称',
-						isGuanzhu: true,
-						title: '我是标题',
-						type: 'img', // img video
-						titlePic: '../../static/demo/datapic/11.jpg',
-						info: {
-							dingNum: 10,
-							caiNum: 10,
-							index: 0,// 0 没有操作过 1 顶过 2 踩过
-						},
-						commentNum: 10, // 评论
-						shareNum:10 // 转发
+						list: [
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: true,
+								title: '我是标题',
+								type: 'img', // img video
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 0,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							},
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: false,
+								title: '我是标题',
+								type: 'video', // img video
+								playNum: '20w',// 播放次数
+								long: "02:00",
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 1,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							}
+						]
 					},
 					{
-						userpic: '../../static/demo/userpic/12.jpg',
-						username: '昵称',
-						isGuanzhu: false,
-						title: '我是标题',
-						type: 'video', // img video
-						playNum: '20w',// 播放次数
-						long: "02:00",
-						titlePic: '../../static/demo/datapic/11.jpg',
-						info: {
-							dingNum: 10,
-							caiNum: 10,
-							index: 1,// 0 没有操作过 1 顶过 2 踩过
-						},
-						commentNum: 10, // 评论
-						shareNum:10 // 转发
+						list: [
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: true,
+								title: '我是标题',
+								type: 'img', // img video
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 0,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							},
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: false,
+								title: '我是标题',
+								type: 'video', // img video
+								playNum: '20w',// 播放次数
+								long: "02:00",
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 1,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							}
+						]
+					},
+					{
+						list: [
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: true,
+								title: '我是标题',
+								type: 'img', // img video
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 0,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							},
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: false,
+								title: '我是标题',
+								type: 'video', // img video
+								playNum: '20w',// 播放次数
+								long: "02:00",
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 1,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							}
+						]
+					},
+					{
+						list: [
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: true,
+								title: '我是标题',
+								type: 'img', // img video
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 0,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							},
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: false,
+								title: '我是标题',
+								type: 'video', // img video
+								playNum: '20w',// 播放次数
+								long: "02:00",
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 1,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							}
+						]
+					},
+					{
+						list: [
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: true,
+								title: '我是标题',
+								type: 'img', // img video
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 0,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							},
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: false,
+								title: '我是标题',
+								type: 'video', // img video
+								playNum: '20w',// 播放次数
+								long: "02:00",
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 1,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							}
+						]
+					},
+					{
+						list: [
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: true,
+								title: '我是标题',
+								type: 'img', // img video
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 0,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							},
+							{
+								userpic: '../../static/demo/userpic/12.jpg',
+								username: '昵称',
+								isGuanzhu: false,
+								title: '我是标题',
+								type: 'video', // img video
+								playNum: '20w',// 播放次数
+								long: "02:00",
+								titlePic: '../../static/demo/datapic/11.jpg',
+								info: {
+									dingNum: 10,
+									caiNum: 10,
+									index: 1,// 0 没有操作过 1 顶过 2 踩过
+								},
+								commentNum: 10, // 评论
+								shareNum:10 // 转发
+							}
+						]
 					}
-				]
+				],
+				
 			}
 		},
 		onLoad() {
-
+			uni.getSystemInfo({
+				success: (res) => {
+					console.log(res.windowHeight)
+					let height = res.windowHeight - uni.upx2px(100) // 导航条是100upx
+					this.contentHeight = height
+				}
+			})
 		}, 
 		methods: {
-
+			tabTap(index){
+				this.tabIndex = Number(index)
+			},
+			swiperChange(e){
+				this.tabIndex = e.detail.current
+			}
 		}
 	}
 </script>
   
 <style lang="stylus">
+.swiper-tab-list{
+	color #969696
+	font-weight bold
+	position relative
+	&.active{
+		color #343434
+		&:after{
+			content: ''
+			position absolute
+			width 70upx
+			background-color #FEDE33
+			height 8upx
+			border-radius 20upx
+			bottom -10upx
+			left calc(50% - 35upx)
+		}
+	}
+}
+::-webkit-scrollbar {
+	display:none;
+	width:0;
+	height:0;
+	color:transparent;
+}
 </style>
