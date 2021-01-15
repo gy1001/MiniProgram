3ada70958c447d7b335589814c7e9885c9bb5004
gaoyuan <gaoyuan_flying@163.com>
2021-01-15 18:10:33 +0800

    小纸条列表组件

---
+++ components/paper-list/paper-list.vue
@@ -0,0 +1,72 @@
+<template>
+	<view class="paper-list u-flex u-acenter">
+		<image class="paper-user-avatar" :src="paperInfo.userPic" mode="widthFix" lazy-load></image>
+		<view class="paper-list-right">
+			<view class="u-flex u-acenter u-jbetween">
+				<view class="paper-list-name">{{paperInfo.userName}}</view>
+				<view class="paper-list-time">{{paperInfo.time}}</view>
+			</view>
+			<view class="u-flex u-acenter u-jbetween">
+				<view class="parer-list-content">{{paperInfo.content}}</view>
+				<template v-if="paperInfo.noReadNum > 0">
+					<uni-badge class="paper-list-badge" type="error" :text="paperInfo.noReadNum"></uni-badge>
+				</template>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import UniBadge from '../../components/uni-badge/uni-badge.vue'
+	export default {
+		components: {
+			'uni-badge': UniBadge
+		},
+		data() {
+			return {
+				
+			};
+		},
+		props: {
+			paperInfo: {
+				type: Object,
+				default: () => {}
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+.paper-list{
+	padding 20upx
+	border-bottom solid 1px #EEEEEE
+	width 100%
+	box-sizing border-box
+	.paper-user-avatar{
+		width 100upx
+		height 100upx
+		border-radius 100%
+		margin-right 20upx
+		flex-shrink 0
+	}
+	.paper-list-right{
+		flex 2
+		.paper-list-name{
+			font-size 32upx
+		}
+		.paper-list-time{
+			color #CBCBCB
+		}
+		.parer-list-content{
+			white-space nowrap
+			overflow hidden
+			text-overflow ellipsis
+			width 450upx
+			color #CBCBCB
+		}
+		.paper-list-badge{
+			flex-shrink 0
+		}
+	}
+}
+</style>
--- pages/paper/paper.vue
+++ pages/paper/paper.vue
@@ -1,67 +1,47 @@
 <template>
 	<view>
 		<!-- 小纸条列表  -->
-		<view class="paper-list u-flex u-acenter">
-			<image class="paper-user-avatar" src="../../static/demo/userpic/16.jpg" mode="widthFix" lazy-load></image>
-			<view class="paper-list-right">
-				<view class="u-flex u-acenter u-jbetween">
-					<view class="paper-list-name">昵称</view>
-					<view class="paper-list-time">10:58</view>
-				</view>
-				<view class="u-flex u-acenter u-jbetween">
-					<view class="parer-list-content">内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</view>
-					<uni-badge class="paper-list-badge" type="primary" text="4"></uni-badge>
-				</view>
-			</view>
-		</view>
+		<block v-for="(paperInfo, paperIndex) in paperList" :key="paperIndex">
+			<paper-list :paperInfo="paperInfo"></paper-list>
+		</block>
 	</view>
 </template>
 
 <script>
-	import UniBadge from '../../components/uni-badge/uni-badge.vue'
+	import PaperList from '../../components/paper-list/paper-list.vue'
 	export default {
 		components: {
-			'uni-badge': UniBadge
+			'paper-list': PaperList
 		},
 		data() {
 			return {
-				
+				paperList:[
+					{
+						userPic: require('../../static/demo/userpic/3.jpg'),
+						userName: '我是名称',
+						time: '10:58',
+						content: '这是内容',
+						noReadNum: 4
+					},
+					{
+						userPic: require('../../static/demo/userpic/2.jpg'),
+						userName: '我是名称1',
+						time: '10:58',
+						content: '这是内容',
+						noReadNum: 1
+					},
+					{
+						userPic: require('../../static/demo/userpic/1.jpg'),
+						userName: '我是名称122',
+						time: '10:58',
+						content: '这是内容',
+						noReadNum: 0
+					}
+				]
 			};
 		}
 	}
 </script>
 
 <style lang="stylus" scoped>
-.paper-list{
-	padding 20upx
-	border-bottom solid 1px #EEEEEE
-	width 100%
-	box-sizing border-box
-	.paper-user-avatar{
-		width 100upx
-		height 100upx
-		border-radius 100%
-		margin-right 20upx
-		flex-shrink 0
-	}
-	.paper-list-right{
-		flex 2
-		.paper-list-name{
-			font-size 32upx
-		}
-		.paper-list-time{
-			color #CBCBCB
-		}
-		.parer-list-content{
-			white-space nowrap
-			overflow hidden
-			text-overflow ellipsis
-			width 450upx
-			color #CBCBCB
-		}
-		.paper-list-badge{
-			flex-shrink 0
-		}
-	}
-}
 </style>
