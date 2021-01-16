78a9cd1822b14609bfac30e48b767b3e6d05a7f0
gaoyuan <gaoyuan_flying@163.com>
2021-01-16 09:24:31 +0800

    聊天底部组件的编写

---
+++ components/user-chat-bottom/user-chat-bottom.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="user-chat-bottom u-flex u-acenter">
+		<input class="user-chat-input" type="text" v-model="chatText" placeholder="文明发言" />
+		<view class="icon iconfont icon-fabu" @click="submitChat"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				chatText: ''
+			};
+		},
+		methods: {
+			submitChat() {
+				this.$emit("submit", this.chatText)
+				this.chatText = ""
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+.user-chat-bottom{
+	position fixed
+	bottom 0
+	left 0
+	height 110upx
+	padding 0 20upx
+	border-top solid 1upx #EEEEEE
+	background-color #FFFFFF
+	width 100%
+	box-sizing border-box
+	.user-chat-input{
+		flex 2
+		margin-right 20upx
+		padding 10upx 20upx
+		border-radius 10upx
+		background-color #F7F7F7
+	}
+	.icon {
+		width 60upx
+		text-align center
+		font-size 45upx
+		color #999999
+	}
+}
+</style>
--- pages/user-chat/user-chat.vue
+++ pages/user-chat/user-chat.vue
@@ -1,15 +1,25 @@
 <template>
 	<view>
 		聊天页面
+		<!-- 输入框 -->
+		<user-chat-bottom @submit="submitChat"></user-chat-bottom>
 	</view>
 </template>
 
 <script>
+	import UserChatBottom from '../../components/user-chat-bottom/user-chat-bottom.vue'
 	export default {
+		components: {
+			'user-chat-bottom': UserChatBottom
+		},
 		data() {
 			return {
-				
 			};
+		},
+		methods: {
+			submitChat(text){
+				console.log(text)
+			}
 		}
 	}
 </script>
