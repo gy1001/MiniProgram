895eded75d8176dd7847c9460199905de294e0da
gaoyuan <gaoyuan_flying@163.com>
2021-01-20 23:33:38 +0800

    糗事的列表以及上拉加载

--- pages/person-space/person-space.vue
+++ pages/person-space/person-space.vue
@@ -8,7 +8,14 @@
 			<view class="gray-space"></view>
 			<swiper-tab-head @tabChange="tabChange" :scrollItemStyle="scrollItemStyle" :tabBars="tabBarList" :tabIndex="tabIndex" :showBorder="false"></swiper-tab-head>
 			<!-- 主页 -->
-			<user-space-info :userInfo="userInfo"></user-space-info>
+			<user-space-info v-if="Number(tabIndex) === 0" :userInfo="userInfo"></user-space-info>
+			<template v-if="Number(tabIndex) === 1">
+				<block v-for="(activeItem, activeIndex) in followListObj.list" :key="activeIndex">
+					<news-list :itemInfo="activeItem"></news-list>
+				</block>
+				<!-- 上拉加载更多 -->
+				<load-more :loadText="followListObj.loadText"></load-more> 
+			</template>
 		</view>
 	</view>
 </template>
@@ -18,12 +25,16 @@
 	import TopBottom from '../../components/top-bottom/top-bottom.vue'
 	import SwiperTabHead from '../../components/swiper-tab-head.vue'
 	import UserSpaceInfo from '../../components/user-space-info/user-space-info.vue'
+	import NewsList from '../../components/news-list/news-list.vue'
+	import LoadMore from '../../components/load-more.vue'
 	export default {
 		components: {
 			"user-space-head": UserSpaceHead,
 			"top-bottom": TopBottom,
 			'swiper-tab-head': SwiperTabHead,
-			"user-space-info": UserSpaceInfo
+			"user-space-info": UserSpaceInfo,
+			"news-list": NewsList,
+			"load-more": LoadMore
 		},
 		data() {
 			return {
@@ -56,13 +67,98 @@
 				],
 				scrollItemStyle: {
 					width : '33.33%'
-				}
+				},
+				followListObj: {
+					list: [
+						{
+							userpic: require('../../static/demo/userpic/12.jpg'),
+							username: '昵称',
+							gender: 1, // 0男1女
+							age: 25,
+							isGuanzhu: true,
+							title: '我是标题',
+							titlePic: require("../../static/demo/datapic/13.jpg"),
+							type: 'video',// video img share
+							videoInfo: {
+								playNum: '20w',
+								duration: '02:00'
+							},
+							address: '深圳龙岗',
+							shareNum: 20,
+							commentNum: 20,
+							isPraise: false,
+							praiseNum: 20
+						},
+						{
+							userpic: require('../../static/demo/userpic/12.jpg'),
+							username: '昵称',
+							gender: 0, // 0男1女
+							age: 25,
+							isGuanzhu: false,
+							title: '我是标题',
+							type: 'img',// video img share
+							titlePic: require("../../static/demo/datapic/13.jpg"),
+							address: '深圳龙岗',
+							shareNum: 20,
+							commentNum: 20,
+							isPraise: true,
+							praiseNum: 20
+						},
+						{
+							userpic: require('../../static/demo/userpic/12.jpg'),
+							username: '昵称',
+							gender: 0, // 0男1女
+							age: 25,
+							isGuanzhu: false,
+							title: '分享标题',
+							shareContent: '分享内容',
+							type: 'share',// video img share
+							titlePic: require("../../static/demo/datapic/13.jpg"),
+							address: '深圳龙岗',
+							shareNum: 20,
+							commentNum: 20,
+							isPraise: false,
+							praiseNum: 20
+						},
+					],
+					loadText: '上拉加载更多'
+				},
 			};
 		},
 		
 		methods: {
 			tabChange(index){
 				this.tabIndex = index
+			},
+			loadMoreActiveList(){
+				const newItemInfo = 	{
+					userpic: require('../../static/demo/userpic/12.jpg'),
+					username: '添加昵称',
+					gender: 0, // 0男1女
+					age: 25,
+					isGuanzhu: false,
+					title: '我是标题',
+					type: 'img',// video img share
+					titlePic: require("../../static/demo/datapic/13.jpg"),
+					address: '深圳龙岗',
+					shareNum: 20,
+					commentNum: 20,
+					isPraise: true,
+					praiseNum: 20
+				}
+				if(this.followListObj.loadText !== "上拉加载更多"){
+					return
+				}
+				this.followListObj.loadText = "加载中..."
+				setTimeout(() => {
+					this.followListObj.list.push(newItemInfo)
+					this.followListObj.loadText = "上拉加载更多"
+				}, 1000);
+			}
+		},
+		onReachBottom() {
+			if(Number(this.tabIndex) === 1){
+				this.loadMoreActiveList()
 			}
 		}
 	}
